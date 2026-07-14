5dbff43fb9c37efa6eaef4e5293e9fa71b79a396
lucadynapower <lchayer@dynapower.com>
2026-07-14 09:16:44 -0400

    Delete PopupChecker.py

--- PopupChecker.py
+++
@@ -1,9 +0,0 @@
-# test_popup.py
-import sys
-from PySide6.QtWidgets import QApplication
-from HandWrittenPythonFiles.MenuPanelWrapper import PopupMenu
-
-app = QApplication(sys.argv)
-window = PopupMenu()
-window.show()
-sys.exit(app.exec())
