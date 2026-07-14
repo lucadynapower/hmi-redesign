14da61e129bcfb48e2084ee504bedc4ded0c332b
lucadynapower <lchayer@dynapower.com>
2026-07-14 09:16:15 -0400

    Delete ControlsChecker.py

--- ControlsChecker.py
+++
@@ -1,9 +0,0 @@
-# test_controls.py
-import sys
-from PySide6.QtWidgets import QApplication
-from HandWrittenPythonFiles.ControlsWidgetWrapper import ControlsWidget
-
-app = QApplication(sys.argv)
-window = ControlsWidget()
-window.show()
-sys.exit(app.exec())
